8a5526cf90da2dd5636f6d859b50b8b7a087b783
villagearun <30830178+villagearun@users.noreply.github.com>
2018-08-02 15:21:09 +0530

    Create holiday_11.c

---
+++ holiday_11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+  char a[100],i,j;
+  scanf("%s",&a);
+     if(a[0]=='s'||a[0]=='S')
+     {
+      printf("yes");
+  }
+  else
+  {
+      printf("no");
+  }
+  return 0;
+}
